507103a7d8faed7f9ea03942708b9c8f74bb7d9b
Erkan Ozgur Yilmaz <eoyilmaz@gmail.com>
2009-10-08 15:16:39 +0300

    oyProjectManager fix: change "non modal" to "window modal"

--- ui/projectManager_UI.py
+++ ui/projectManager_UI.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file '/home/ozgur/maya/scripts/oy-maya-scripts/oyTools/oyProjectManager/ui/projectManager.ui'
 #
-# Created: Thu Oct  8 14:42:44 2009
+# Created: Thu Oct  8 15:15:41 2009
 #      by: PyQt4 UI code generator 4.4.4
 #
 # WARNING! All changes made in this file will be lost!
@@ -12,6 +12,7 @@ from PyQt4 import QtCore, QtGui
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
+        MainWindow.setWindowModality(QtCore.Qt.WindowModal)
         MainWindow.resize(481, 328)
         sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Preferred, QtGui.QSizePolicy.Preferred)
         sizePolicy.setHorizontalStretch(1)
