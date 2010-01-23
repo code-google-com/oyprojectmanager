9798444d7ab70a8e9fcf40ca9910496c4f00e0c3
Erkan Ozgur Yilmaz <eoyilmaz@gmail.com>
2010-01-23 13:38:07 +0200

    oyProjectManager: v10.1.23 - mayaEnv: fixed setting of the playblast folder path, it should be an absolute   path to the playblast file - assetManager: fixed a little bug in export which causing a little error but,   doesn't interrupt the export process

---
+++ environments/houdiniEnv.py
@@ -0,0 +1,369 @@
+"""
+houdiniEnv.py by Erkan Ozgur Yilmaz (c) 2009-2010
+
+Description:
+------------
+the houdini environment part of the asset management system...
+
+has commands to save, open and import houdini files
+
+"""
+
+
+
+import os
+import hou
+import re
+from oyProjectManager.dataModels import assetModel, projectModel, repositoryModel, abstractClasses
+import oyAuxiliaryFunctions as oyAux
+
+
+
+__version__ = "10.1.5"
+
+
+
+
+
+
+########################################################################
+class HoudiniEnvironment(abstractClasses.Environment):
+    """the houdini environment class
+    """
+    
+    #----------------------------------------------------------------------
+    def save(self):
+        """the save action for houdini environment
+        """
+        
+        # set the extension to hip
+        self._asset.setExtension('hip')
+        
+        # create the folder if it doesn't exists
+        oyAux.createFolder( self._asset.getPath() )
+        
+        # houdini uses / instead of \ under windows
+        # lets fix it
+        
+        fullPath = self._asset.getFullPath()
+        fullPath = fullPath.replace('\\','/')
+        
+        # houdini accepts only strings as file name, no unicode support as I see
+        hou.hipFile.save( file_name = str(fullPath) )
+        
+        # set the environment variables
+        self.setEnvironmentVariables()
+        
+        return True
+    
+    
+    
+    #----------------------------------------------------------------------
+    def open_(self, force=False ):
+        """the open action for houdini environment
+        """
+        
+        if hou.hipFile.hasUnsavedChanges() and not force:
+            raise RuntimeError
+        
+        fullPath = self._asset.getFullPath()
+        fullPath = fullPath.replace('\\','/')
+        
+        hou.hipFile.load( file_name = str(fullPath) , suppress_save_prompt=True )
+        
+        # set the environment variables
+        self.setEnvironmentVariables()
+        
+        return True
+    
+    
+    
+    #----------------------------------------------------------------------
+    def import_(self):
+        """the import action for houdini environment
+        """
+        
+        fullPath = self._asset.getFullPath()
+        fullPath = fullPath.replace('\\','/')
+        
+        hou.hipFile.merge( str(fullPath) )
+        
+        return True
+    
+    
+    
+    #----------------------------------------------------------------------
+    def getPathVariables(self):
+        """gets the file name from houdini environment
+        """
+        
+        foundValidAsset = False
+        readRecentFile = True
+        fileName = path = None
+        
+        #db = projectModel.Database()
+        repo = repositoryModel.Repository()
+        
+        fullPath = hou.hipFile.name()
+        
+        # unfix windows path
+        if os.name == 'nt':
+            fullPath = fullPath.replace('/','\\')
+        
+        
+        if fullPath != 'untitled.hip':
+            fileName = os.path.basename( fullPath )
+            path = os.path.dirname( fullPath )
+            
+            # try to create an asset with that info
+            projName, seqName = repo.getProjectAndSequenceNameFromFilePath( fullPath )
+            
+            proj = projectModel.Project( projName )
+            seq = projectModel.Sequence( proj, seqName )
+            
+            testAsset = assetModel.Asset( proj, seq, fileName )
+            
+            #if path != '':
+                #fileName = os.path.basename( fullPath )
+            #else:
+                #path = None
+            if testAsset.isValidAsset():
+                fileName = testAsset.getFileName()
+                path = testAsset.getPath()
+                readRecentFile = False
+        
+        if readRecentFile:
+            # there is no file oppend yet use the first valid file
+            # from the recent files list
+            
+            recentFiles = self.getRecentFileList()
+            
+            # unfix the windows paths from / to \\
+            if os.name == 'nt':
+                recentFiles = [ recentFile.replace('/','\\') for recentFile in recentFiles ]
+            
+            for i in range(len(recentFiles)-1,0,-1):
+                
+                fileName = os.path.basename( recentFiles[i] )
+                projName, seqName = repo.getProjectAndSequenceNameFromFilePath( recentFiles[i] )
+                
+                if projName != None and seqName != None:
+                    
+                    proj = projectModel.Project( projName )
+                    seq = projectModel.Sequence( proj, seqName )
+                    
+                    testAsset = assetModel.Asset( proj, seq, fileName )
+                    
+                    if testAsset.isValidAsset():
+                        path = testAsset.getPath()
+                        foundValidAsset = True
+                        break
+            
+    ##        # get the workspace path
+    ##        workspacePath = os.getenv('JOB')
+    ##        returnWorkspace = False
+    ##        
+    ##        workspaceIsValid = False
+    ##        
+    ##        if workspacePath != '' and workspacePath != None:
+    ##            workspaceIsValid = True
+    ##        
+    ##        if foundValidAsset:
+    ##            #print "found a valid asset with path", path
+    ##            
+    ##            # check if teh recent files path matches the current workspace
+    ##            if not path.startswith( workspacePath ) and workspaceIsValid:
+    ##                # use the workspacePath
+    ##                returnWorkspace = True
+    ##        else:
+    ##            # just get the path from workspace and return an empty fileName
+    ##            returnWorkspace = True
+    ##        
+    ##        if returnWorkspace and workspaceIsValid:
+    ##            fileName = None
+    ##            path = workspacePath
+        
+        return fileName, path
+    
+    
+    
+    #----------------------------------------------------------------------
+    def setEnvironmentVariables(self):
+        """sets the environment variables according to the given assetObject
+        """
+        # set the $JOB variable to the sequence root
+        os.environ['JOB'] = str( self._asset.getSequenceFullPath()).replace('\\','/')
+    
+    
+    
+    #----------------------------------------------------------------------
+    def getRecentFileList(self):
+        """returns the recent HIP files list from the houdini
+        """
+        
+        # use a FileHistory object
+        fHist = FileHistory()
+        
+        # get the hip files list
+        recentHipFiles = fHist.getRecentFiles('HIP')
+        
+        return recentHipFiles
+    
+    
+    
+    #----------------------------------------------------------------------
+    def getFrameRange(self):
+        """returns the frame range of the
+        """
+        # use the hscript commands to get the frame range
+        timeInfo = hou.hscript('tset')[0].split('\n')
+        
+        pattern = r'[0-9\.]+'
+        
+        startFrame = int( hou.timeToFrame( float( re.search( pattern, timeInfo[2] ).group(0) ) ) )
+        duration = int( re.search( pattern, timeInfo[0] ).group(0) )
+        endFrame = startFrame + duration - 1
+        
+        return startFrame, endFrame
+    
+    
+    
+    #----------------------------------------------------------------------
+    def setFrameRange(self, startFrame=1, endFrame=100):
+        """sets the frame range
+        """
+        
+        
+        # --------------------------------------------
+        # set the timeline
+        currFrame = hou.frame()
+        if currFrame < startFrame:
+            hou.setFrame( startFrame )
+        elif currFrame > endFrame:
+            hou.setFrame( endFrame )
+        
+        # for now use hscript, the python version is not implemented yet
+        hou.hscript('tset `('+ str(startFrame) +'-1)/$FPS` `'+ str(endFrame)+'/$FPS`')
+        
+        
+        # --------------------------------------------
+        # Set the render nodes frame ranges if any
+        # get the out nodes
+        ropContext = hou.node('/out')
+        
+        # get the children
+        outNodes = ropContext.children()
+        
+        if len(outNodes):
+            for outNode in outNodes:
+                outNode.setParms( {'trange':1,'f1':startFrame,'f2':endFrame,'f3':1})
+        
+
+
+
+
+
+
+########################################################################
+class FileHistory( abstractClasses.Singleton ):
+    """a houdini recent file history parser
+    
+    holds the data in a dictionary, where the keys are the file types and the
+    values are string list of recent file paths of that type
+    """
+    
+    #----------------------------------------------------------------------
+    def __init__(self):
+        self._historyFileName = 'file.history'
+        
+        self._historyFilePath = ''
+        
+        if os.name == 'nt':
+            # under windows the HIH is useless
+            # interpret the HIH from POSE environment variable
+            
+            self._historyFilePath = os.path.dirname( os.getenv('POSE') )
+        else:
+            self._historyFilePath = os.getenv('HIH')
+        
+        self._historyFileFullPath = os.path.join( self._historyFilePath, self._historyFileName )
+        
+        self._buffer =  []
+        
+        self._history = dict()
+        
+        self._read()
+        self._parse()
+    
+    
+    
+    #----------------------------------------------------------------------
+    def _read(self):
+        """reads the history file to a buffer
+        """
+        try:
+            historyFile = open( self._historyFileFullPath )
+        except IOError:
+            self._buffer = []
+            return
+        
+        self._buffer = historyFile.readlines()
+        
+        # strip all the lines
+        self._buffer = [ line.strip() for line in self._buffer ]
+        
+        historyFile.close()
+    
+    
+    
+    #----------------------------------------------------------------------
+    def _parse(self):
+        """parses the data in self._buffer
+        """
+        
+        self._history = dict()
+        
+        bufferList = self._buffer
+        
+        keyName = ''
+        pathList = []
+        
+        lenBuffer = len(bufferList)
+        
+        for i in range(lenBuffer):
+            
+            # try to find a '{'
+            if bufferList[i] == '{':
+                # create a key with the previous line
+                keyName = bufferList[i-1]
+                pathList = []
+                
+                # starting from the next line
+                for j in range(i+1, lenBuffer ):
+                    
+                    # add all the lines to the pathList until you find a '}'
+                    currentElement = bufferList[j]
+                    if  currentElement != '}':
+                        pathList.append( currentElement )
+                    else:
+                        # set i to j+1 and let it continue
+                        i = j + 1
+                        break
+                
+                # append the key and data to the dictionary
+                self._history[ keyName ] = pathList
+    
+    
+    
+    #----------------------------------------------------------------------
+    def getRecentFiles(self, typeName=''):
+        """returns the file list of the given file type
+        """
+        
+        if typeName == '' or typeName == None:
+            return []
+        else:
+            if self._history.has_key( typeName ):
+                return self._history[ typeName ]
+            else:
+                return []
---
+++ ui/assetUpdater.py
@@ -0,0 +1,327 @@
+import os, sys
+import oyAuxiliaryFunctions as oyAux
+from PyQt4 import QtGui, QtCore
+import assetUpdater_UI
+
+import oyProjectManager
+from oyProjectManager.dataModels import assetModel, projectModel, repositoryModel
+from oyProjectManager.ui import singletonQapplication
+
+
+__version__ = "9.12.26"
+
+
+
+#----------------------------------------------------------------------
+def UI( environmentName=None, parent=None ):
+    """the UI
+    """
+    global app
+    global mainWindow
+    app = singletonQapplication.QApplication(sys.argv)
+    mainWindow = MainWindow( environmentName, parent )
+    mainWindow.show()
+    app.setStyle('Plastique')
+    app.exec_()
+    app.connect(app, QtCore.SIGNAL("lastWindowClosed()"), app, QtCore.SLOT("quit()"))
+
+
+
+
+
+
+#######################################################################
+class MainWindow(QtGui.QMainWindow, assetUpdater_UI.Ui_MainWindow):
+    """the main dialog of the asset updater system
+    
+    the assetTuple list consist of an asset object and a reference object
+    
+    for Maya environment the reference object is the PyMel Reference node,
+    for other environments reference object type will be as native as it can be
+    """
+    
+    
+    
+    #----------------------------------------------------------------------
+    def __init__(self, environmentName=None, parent=None):
+        QtGui.QMainWindow.__init__(self, parent)
+        self.setupUi(self)
+        
+        # change the window title
+        self.setWindowTitle( self.windowTitle() + ' v' + __version__ )
+        
+        # center to the window
+        self._centerWindow()
+        
+        self._horizontalLabels = [ 'Asset Name', 'Current', 'Last', 'Do Update?' ]
+        self.assetList_tableWidget.setHorizontalHeaderLabels( self._horizontalLabels )
+        
+        # ------------------------------------------------------------
+        # SIGNALS
+        # ------------------------------------------------------------
+        
+        # cancel button
+        QtCore.QObject.connect( self.cancel_pushButton, QtCore.SIGNAL("clicked()"), self.close )
+        
+        # select all button
+        QtCore.QObject.connect( self.selectAll_pushButton, QtCore.SIGNAL("clicked()"), self._selectAllAssets )
+        
+        # select none button
+        QtCore.QObject.connect( self.selectNone_pushButton, QtCore.SIGNAL("clicked()"), self._selectNoAsset )
+        
+        # update button
+        QtCore.QObject.connect( self.update_pushButton, QtCore.SIGNAL("clicked()"), self.updateAssets )
+        
+        # ------------------------------------------------------------
+        
+        self._assetTupleList = []
+        self._numOfAssets = 0
+        
+        self._tableItems = []
+        
+        
+        # ------------------------------------------------------------
+        # setup the environment
+        # ------------------------------------------------------------
+        self._environmentName = environmentName
+        self._environment = None
+        
+        self.setEnvironmentName( self._environmentName )
+        
+        ## ---------------
+        ## debug
+        ## show the assets in the interface
+        #self._testDataFill()
+        ## ---------------
+        self._doEnvRead()
+        self._fillUI()
+    
+    
+    
+    #----------------------------------------------------------------------
+    def _centerWindow(self):
+        """centers the window to the screen
+        """
+        screen = QtGui.QDesktopWidget().screenGeometry()
+        size =  self.geometry()
+        self.move((screen.width()-size.width())/2, (screen.height()-size.height())/2)
+    
+    
+    
+    #----------------------------------------------------------------------
+    def getAssetTupleListFromEnvironment(self):
+        """gets the references from environment
+        
+        returns a tuple consist of an asset and the environments representation
+        of the asset
+        """
+        return self._environment.checkReferenceVersions()
+    
+    
+    #----------------------------------------------------------------------
+    def getAssetTupleList(self):
+        """returns the asset tuple list
+        """
+        return self._assetTupleList
+    
+    
+    
+    #----------------------------------------------------------------------
+    def setAssetTupleList(self, assetTupleList):
+        """sets the asset tuple list
+        """
+        self._assetTupleList = assetTupleList
+        self._numOfAssets = len( self._assetTupleList )
+    
+    assetTupleList = property( getAssetTupleList, setAssetTupleList )
+    
+    
+    
+    #----------------------------------------------------------------------
+    def getEnvironmentName(self):
+        """returns the environment name
+        """
+        return self._environmentName
+    
+    
+    
+    #----------------------------------------------------------------------
+    def setEnvironmentName(self, environmentName):
+        """sets the environment name
+        """
+        self._environmentName = environmentName
+        
+        if self._environmentName == 'MAYA':
+            from oyProjectManager.environments import mayaEnv
+            self.setEnvironment( mayaEnv.MayaEnvironment() )
+        
+        elif self._environmentName == 'HOUDINI':
+            from oyProjectManager.environments import houdiniEnv
+            self.setEnvironment( houdiniEnv.HoudiniEnvironment() )
+        
+        elif self._environmentName == 'NUKE':
+            from oyProjectManager.environments import nukeEnv
+            self.setEnvironment(  nukeEnv.NukeEnvironment() )
+    
+    environmentName = property( getEnvironmentName, setEnvironmentName )
+    
+    
+    
+    #----------------------------------------------------------------------
+    def getEnvironment(self):
+        """returns the environment object
+        """
+        return self._environment
+    
+    
+    
+    #----------------------------------------------------------------------
+    def setEnvironment(self, environment):
+        """sets the environment
+        """
+        self._environment = environment
+    
+    environment = property( getEnvironment, setEnvironment )
+    
+    
+    
+    ##----------------------------------------------------------------------
+    #def _testDataFill(self):
+        #"""creates some test data to test the interface
+        #"""
+        
+        ## create a project a sequence and get some assets
+        #assetCount = 10
+        
+        #repo = repositoryModel.Repository()
+        #proj = projectModel.Project( 'ETI_TOPKEK' )
+        #seq = projectModel.Sequence( proj, '_CHARACTER_SETUP_' )
+        
+        #assetFileNames = seq.getAllAssetFileNamesForType('RIG')
+        
+        ## fill the data
+        #self._assetTupleList = []
+        #for i in range(assetCount):
+            #self._assetTupleList.append( (assetModel.Asset( proj, seq, assetFileNames[i] ), None) )
+    
+    
+    
+    #----------------------------------------------------------------------
+    def _fillUI(self):
+        """fills the UI with the asset data
+        """
+        
+        # set the row count
+        self.assetList_tableWidget.setRowCount( self._numOfAssets)
+        
+        for i,assetTuple in enumerate(self._assetTupleList):
+            #assert(isinstance(asset, assetModel.Asset) )
+            
+            asset = assetTuple[0]
+            
+            # ------------------------------------
+            # the critique name
+            assetName_tableWI = QtGui.QTableWidgetItem( asset._getCritiqueName() )
+            # align to left and vertical center
+            assetName_tableWI.setTextAlignment( 0x0001 | 0x0080  )
+            self.assetList_tableWidget.setItem( i, 0, assetName_tableWI )
+            # ------------------------------------
+            
+            
+            # ------------------------------------
+            # current version
+            currentVersionNumber = str(asset.getVersionNumber() )
+            currentVersion_tableWI = QtGui.QTableWidgetItem( currentVersionNumber )
+            # align to horizontal and vertical center
+            currentVersion_tableWI.setTextAlignment( 0x0004 | 0x0080  )
+            self.assetList_tableWidget.setItem( i, 1, currentVersion_tableWI )
+            # ------------------------------------
+            
+            
+            # ------------------------------------
+            # last version
+            lastVersionNumber = str( asset.getLatestVersion2()[1] )
+            lastVersion_tableWI = QtGui.QTableWidgetItem( lastVersionNumber )
+            # align to horizontal and vertical center
+            lastVersion_tableWI.setTextAlignment( 0x0004 | 0x0080 )
+            self.assetList_tableWidget.setItem( i, 2, lastVersion_tableWI )
+            # ------------------------------------
+            
+            
+            # ------------------------------------
+            # do update ?
+            checkBox_tableWI = QtGui.QTableWidgetItem('')
+            #checkBox_tableWI.setCheckState( 16 )
+            checkBox_tableWI.setCheckState( 0 )
+            self.assetList_tableWidget.setItem( i, 3, checkBox_tableWI )
+            # ------------------------------------
+            
+            self._tableItems.append( [assetName_tableWI, currentVersion_tableWI, lastVersion_tableWI, checkBox_tableWI] )
+    
+    
+    
+    #----------------------------------------------------------------------
+    def _doEnvRead(self):
+        """gets the asset tuple from env
+        """
+        self._assetTupleList = self.getAssetTupleListFromEnvironment()
+        self._numOfAssets = len( self._assetTupleList )
+    
+    
+    
+    #----------------------------------------------------------------------
+    def _selectAllAssets(self):
+        """selects all the assets in the tableWidget
+        """
+        
+        for currentItems in self._tableItems:
+            currentItem = currentItems[3]
+            currentItem.setCheckState( 2 )
+    
+    
+    #----------------------------------------------------------------------
+    def _selectNoAsset(self):
+        """deselects all assets in the tableWidget
+        """
+        
+        for currentItems in self._tableItems:
+            currentItem = currentItems[3]
+            currentItem.setCheckState( 0 )
+        
+    
+    
+    
+    #----------------------------------------------------------------------
+    def updateAssets(self):
+        """updates the assets if it is checked in the
+        """
+        
+        # get the marked assets from UI first
+        markedAssets = self.getMarkedAssets()
+        
+        # send them back to environment
+        self._environment.updateAssets( markedAssets )
+        
+        # close the interface
+        self.close()
+    
+    
+    
+    #----------------------------------------------------------------------
+    def getMarkedAssets(self):
+        """returns the assets as tuple again, if it is checked in the interface
+        """
+        
+        markedAssetList = []
+        
+        # find the marked assets
+        for i in range(self._numOfAssets):
+            checkBox_tableItem = self._tableItems[i][3]
+            assert(isinstance(checkBox_tableItem, QtGui.QTableWidgetItem))
+            
+            if checkBox_tableItem.checkState() == 2:
+                # get the ith number of the asset
+                markedAssetList.append( self._assetTupleList[i] )
+        
+        return markedAssetList
+        
