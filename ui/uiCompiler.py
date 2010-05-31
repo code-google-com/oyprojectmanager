cb9590d89a444630ea34c4013d59c21e0b361916
Erkan Ozgur Yilmaz <eoyilmaz@gmail.com>
2010-05-31 17:37:10 +0300

    added new user and update userSetup.mel

---
+++ ui/uiCompiler.py
@@ -0,0 +1,33 @@
+from PyQt4 import uic, QtCore, QtGui
+
+version = '10.4.28'
+
+global uicFile
+global pyFile
+
+uicFilePaths = []
+pyFilePaths = []
+
+uicFilePaths.append( '/home/ozgur/maya/scripts/oy-maya-scripts/oyTools/oyProjectManager/ui/assetManager.ui' )
+pyFilePaths.append( '/home/ozgur/maya/scripts/oy-maya-scripts/oyTools/oyProjectManager/ui/assetManager_UI.py' )
+
+#uicFilePaths.append( '/home/ozgur/maya/scripts/oy-maya-scripts/oyTools/oyProjectManager/ui/projectManager.ui' )
+#pyFilePaths.append( '/home/ozgur/maya/scripts/oy-maya-scripts/oyTools/oyProjectManager/ui/projectManager_UI.py' )
+
+#uicFilePaths.append( '/home/ozgur/maya/scripts/oy-maya-scripts/oyTools/oyProjectManager/ui/assetUpdater.ui' )
+#pyFilePaths.append( '/home/ozgur/maya/scripts/oy-maya-scripts/oyTools/oyProjectManager/ui/assetUpdater_UI.py' )
+
+#uicFilePaths.append( '/home/ozgur/maya/scripts/oy-maya-scripts/oyTools/oyProjectManager/ui/shotEditor.ui' )
+#pyFilePaths.append( '/home/ozgur/maya/scripts/oy-maya-scripts/oyTools/oyProjectManager/ui/shotEditor_UI.py' )
+
+#uicFilePaths.append( '/home/ozgur/maya/scripts/oy-maya-scripts/oyTools/oyProjectManager/ui/projectSetter.ui' )
+#pyFilePaths.append( '/home/ozgur/maya/scripts/oy-maya-scripts/oyTools/oyProjectManager/ui/projectSetter_UI.py' )
+
+
+
+for i,uicFilePath in enumerate(uicFilePaths):
+    uicFile = file( uicFilePath)
+    pyFile  = file( pyFilePaths[i],'w')
+    uic.compileUi( uicFile, pyFile )
+    uicFile.close()
+    pyFile.close()
