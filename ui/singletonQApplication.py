aaa8fc7d12c7aed2a9f15a44db9e3446b95bb17a
Erkan Ozgur Yilmaz <eoyilmaz@gmail.com>
2010-06-21 15:38:59 +0300

    changed the default project settings to include FX asset type

---
+++ ui/singletonQApplication.py
@@ -0,0 +1,17 @@
+from PyQt4 import QtGui
+
+
+
+########################################################################
+class QApplication( QtGui.QApplication ):
+    """a singleton QApplication class
+    """
+    
+    #----------------------------------------------------------------------
+    def __new__(cls, *args):
+        if cls.instance() == None:
+            # create a QApplication
+            QtGui.QApplication(*args)
+        
+        # return the instance
+        return cls.instance()
