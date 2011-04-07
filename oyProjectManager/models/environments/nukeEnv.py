3fe78a1680d8181028f3ce4cae35454815b04a6f
Erkan Ozgur Yilmaz <eoyilmaz@gmail.com>
2011-04-08 00:35:22 +0300

    v11.4.7 -------
     * :class:`~oyProjectManager.models.abstractClasses.Environment` now raises
       notImplemented errors for the methods which are not implemented by the child
       class.

---
+++ oyProjectManager/models/environments/nukeEnv.py
@@ -0,0 +1,311 @@
+# -*- coding: utf-8 -*-
+
+
+
+import os
+import nuke
+import oyAuxiliaryFunctions as oyAux
+from oyProjectManager.models import asset, project, abstractClasses, repository
+
+
+
+
+
+
+########################################################################
+class NukeEnvironment(abstractClasses.Environment):
+    """the nuke environment class
+    """
+    
+    
+    
+    #----------------------------------------------------------------------
+    def __init__(self, asset=None, name='', extensions=None ):
+        """nuke spesific init
+        """
+        # call the supers __init__
+        super(NukeEnvironment, self).__init__(asset, name, extensions)
+        
+        # and add you own modifications to __init__
+        # get the root node
+        self._root = self.getRootNode()
+        
+        self._main_output_node_name = "MAIN_OUTPUT"
+    
+    
+    
+    #----------------------------------------------------------------------
+    def getRootNode(self):
+        """returns the root node of the current nuke session
+        """
+        
+        return nuke.toNode("root")
+    
+    
+    
+    #----------------------------------------------------------------------
+    def save(self):
+        """"the save action for nuke environment
+        
+        uses Nukes own python binding
+        """
+        
+        # set the extension to 'nk'
+        self._asset.extension = 'nk'
+        
+        fullPath = self._asset.fullPath
+        fullPath = fullPath.replace('\\','/')
+        
+        # create the main write node
+        self.create_main_write_node()
+        
+        # replace read and write node paths
+        self.replace_file_path()
+        
+        nuke.scriptSaveAs( fullPath )
+        
+        return True
+    
+    
+    
+    #----------------------------------------------------------------------
+    def export(self):
+        """the export action for nuke environment
+        """
+        
+        # set the extension to 'nk'
+        self._asset.extension = 'nk'
+        
+        fullPath = self._asset.fullPath
+        
+        # replace \\ with /
+        fullPath = fullPath.replace('\\','/')
+        
+        nuke.nodeCopy( fullPath )
+        
+        return True
+    
+    
+    
+    #----------------------------------------------------------------------
+    def open_(self, force=False):
+        """the open action for nuke environment
+        """
+        
+        fullPath = self._asset.fullPath
+        
+        # replace \\ with /
+        fullPath = fullPath.replace('\\','/')
+        
+        nuke.scriptOpen(fullPath)
+        
+        # replace paths
+        self.replace_file_path()
+        
+        return True
+    
+    
+    
+    #----------------------------------------------------------------------
+    def import_(self):
+        """the import action for nuke environment
+        """
+        
+        fullPath = self._asset.fullPath
+        
+        # replace \\ with /
+        fullPath = fullPath.replace('\\','/')
+        
+        nuke.nodePaste( fullPath )
+        return True
+    
+    
+    
+    #----------------------------------------------------------------------
+    def getPathVariables(self):
+        """gets the file name from nuke
+        """
+        
+        fullPath = path = fileName = None
+        fullPath = self._root.knob('name').value()
+        
+        
+        if fullPath != None and fullPath != '':
+            # for winodws replace the path seperator
+            if os.name == 'nt':
+                fullPath = fullPath.replace('/','\\')
+            
+            fileName = os.path.basename( fullPath )
+            path = os.path.dirname( fullPath )
+        else:
+            
+            # use the last file from the recent file list
+            fullPath = nuke.recentFile(1)
+            
+            # for winodws replace the path seperator
+            if os.name == 'nt':
+                fullPath = fullPath.replace('/','\\')
+            
+            fileName = os.path.basename( fullPath )
+            path = os.path.dirname( fullPath )
+            
+        return fileName, path
+    
+    
+    
+    #----------------------------------------------------------------------
+    def getFrameRange(self):
+        """returns the current frame range
+        """
+        #self._root = self.getRootNode()
+        startFrame = int(self._root.knob('first_frame').value())
+        endFrame = int(self._root.knob('last_frame').value())
+        return startFrame, endFrame
+    
+    
+    
+    #----------------------------------------------------------------------
+    def setFrameRange(self, startFrame=1, endFrame=100):
+        """sets the start and end frame range
+        """
+        #self._root = self.getRootNode()
+        self._root.knob('first_frame').setValue(startFrame)
+        self._root.knob('last_frame').setValue(endFrame)
+    
+    
+    #----------------------------------------------------------------------
+    def setTimeUnit(self, timeUnit='pal'):
+        """sets the current time unit
+        """
+        
+        # get the fps value of the given time unit
+        repo = repository.Repository()
+        
+        try:
+            fps = repo.timeUnits[ timeUnit ]
+        except KeyError:
+            # set it to pal by default
+            fps = repo.timeUnits[ 'pal' ]
+        
+        self._root.knob('fps').setValue( float(fps) )
+    
+    
+    
+    #----------------------------------------------------------------------
+    def getTimeUnit(self):
+        """returns the current time unit
+        """
+        
+        currentFps = str(int(self._root.knob('fps').getValue()))
+        
+        repo = repository.Repository()
+        timeUnits = repo.timeUnits
+        
+        # by default set it to pal
+        timeUnit = 'pal'
+        
+        for timeUnitName, timeUnitFps in timeUnits.iteritems():
+            if currentFps == timeUnitFps:
+                timeUnit = timeUnitName
+                break
+        
+        return timeUnit
+    
+    
+    
+    #----------------------------------------------------------------------
+    def get_main_write_node(self):
+        """Returns the main write node in the scene or None.
+        """
+        
+        # list all the write nodes in the current file
+        all_write_nodes = nuke.allNodes("Write")
+        
+        main_write_node = None
+        
+        for write_node in all_write_nodes:
+            if write_node.name().startswith(self._main_output_node_name):
+                main_write_node = write_node
+                return main_write_node
+        
+        return None
+    
+    
+    
+    #----------------------------------------------------------------------
+    def create_main_write_node(self):
+        """creates the default write node if there is no one created before.
+        """
+        
+        # list all the write nodes in the current file
+        main_write_node = self.get_main_write_node()
+        
+        if main_write_node is None:
+            # create one with correct output path
+            main_write_node = nuke.nodes.Write()
+            main_write_node.setName(self._main_output_node_name)
+        
+        # set the output path
+        
+        assert(isinstance(self._asset, asset.Asset))
+        
+        seq = self._asset.sequence
+        assert(isinstance(seq, project.Sequence))
+        
+        aType = self._asset.type
+        assert(isinstance(aType, asset.AssetType))
+        
+        
+        output_file_name = self._asset.baseName + "_" + \
+                           self._asset.subName + "_" + \
+                           "OUTPUT_" + \
+                           self._asset.revisionString + "_" + \
+                           self._asset.versionString + "_" + \
+                           self._asset.userInitials + ".###.tga"
+        
+        output_file_full_path = os.path.join(
+            seq.fullPath,
+            self._asset.type.output_path,
+            output_file_name
+        )
+        
+        main_write_node["file"].setValue(output_file_full_path)
+        
+        # set the output file type to targa
+        main_write_node["file_type"].setValue(13)
+        main_write_node["channels"].setValue("rgba")
+    
+    
+    
+    #----------------------------------------------------------------------
+    def replace_file_path(self):
+        """replaces file paths with environment variable scripts
+        """
+        
+        repo = repository.Repository()
+        
+        env_str = "[getenv " + repo.repository_path_env_key + "]"
+        
+        # convert the given path to tcl environment script
+        def repPath(path):
+            return path.replace(repo.serverPath, env_str)
+        
+        # get all read nodes
+        allNodes = nuke.allNodes()
+        
+        readNodes = [node for node in allNodes if node.Class() == "Read"]
+        writeNodes = [node for node in allNodes if node.Class() == "Write"]
+        readGeoNodes = [node for node in allNodes if node.Class() == "ReadGeo"]
+        readGeo2Nodes = [node for node in allNodes if node.Class() == "ReadGeo2"]
+        writeGeoNodes = [node for node in allNodes if node.Class() == "WriteGeo"]
+        
+        def nodeRep(nodes):
+            [node["file"].setValue(
+                repPath(node["file"].getValue())
+            ) for node in nodes]
+        
+        nodeRep(readNodes)
+        nodeRep(writeNodes)
+        nodeRep(readGeoNodes)
+        nodeRep(readGeo2Nodes)
+        nodeRep(writeGeoNodes)
---
+++ tests/models/test_sequence.py
@@ -0,0 +1,181 @@
+# -*- coding: utf-8 -*-
+
+
+
+import os, sys, shutil
+import unittest
+from xml.dom import minidom
+from oyProjectManager.models import project, repository, asset
+
+
+
+
+
+
+########################################################################
+class SequenceTester(unittest.TestCase):
+    """tests the Sequence class
+    """
+    
+    
+    
+    #----------------------------------------------------------------------
+    @classmethod
+    def setUpClass(cls):
+        """set up the test in class level
+        """
+        
+        # setup environment variable for default settings
+        
+        import os, sys
+        import oyProjectManager
+        
+        oyProjectManager_path = os.path.sep.join(
+            oyProjectManager.__file__.split(os.path.sep)[:-2]
+        )
+        
+        test_settings_path = os.path.join(oyProjectManager_path,
+                                          "tests/test_settings")
+        
+        # append or update the environment key to point the test_settings path
+        os.environ["OYPROJECTMANAGER_SETTINGS_PATH"] = test_settings_path
+    
+    
+    
+    #----------------------------------------------------------------------
+    def setUp(self):
+        """set up the per test level
+        """
+        
+        self.created_projects = []
+    
+    
+    
+    #----------------------------------------------------------------------
+    def tearDown(self):
+        """clean the tests
+        """
+        
+        # clean up projects
+        for proj in self.created_projects:
+            shutil.rmtree(proj.fullPath)
+        
+    
+    
+    
+    #----------------------------------------------------------------------
+    def test_setup_is_working_fine(self):
+        """testing if test setup is working fine
+        """
+        
+        # now create a repository and ask the server path and check if it
+        # matches the test_settings
+        repo = repository.Repository()
+        
+        # BUG: works only under linux fix it later
+        self.assertEquals(repo.serverPath, "/tmp/JOBs")
+    
+    
+    
+    #----------------------------------------------------------------------
+    def test_if_a_newly_created_sequence_dont_have_output_folders_object(self):
+        """testing if a newly created sequence doesn't have any output_folders
+        node in its settings
+        """
+        
+        # create a project
+        test_proj = project.Project("TEST_PROJECT")
+        test_proj.create()
+        
+        self.created_projects.append(test_proj)
+        
+        # create a sequence
+        test_seq = test_proj.createSequence("TEST_SEQ", "1")
+        
+        # now get the settings path and check if there is a node called
+        # output folders by using the minidom
+        document = minidom.parse(
+            os.path.join(test_seq.fullPath, ".settings.xml")
+        )
+        
+        output_folders = document.getElementsByTagName("output_folders")
+        
+        self.assertEquals(output_folders, [])
+    
+    
+    
+    #----------------------------------------------------------------------
+    def test_if_an_old_sequence_with_and_old_settings_is_parsed_correctly(self):
+        """testing if an old sequence which has an output_folders node is
+        parsed without any problem
+        """
+        
+        # create a new project and by using the minidom add output_folders node
+        # and some output nodes as children
+        
+        # create a project
+        test_proj = project.Project("TEST_PROJECT")
+        test_proj.create()
+        
+        self.created_projects.append(test_proj)
+        
+        # create a sequence
+        test_seq = test_proj.createSequence("TEST_SEQ", "1")
+        
+        # now by using the minidom add output_folders to the structure node
+        settingsFileFullPath = os.path.join(test_seq.fullPath, ".settings.xml")
+        
+        settings = minidom.parse(settingsFileFullPath)
+        
+        outputFoldersNode = minidom.Element("outputFolders")
+        
+        # move all the output_path information to outputFolders node
+        
+        # and remove the output_path from any type node under the assetTypes
+        # node
+        assetTypesNode = settings.getElementsByTagName("assetTypes")[0]
+        assert(isinstance(assetTypesNode, minidom.Element))
+        
+        for child in assetTypesNode.getElementsByTagName("type"):
+            assert(isinstance(child, minidom.Element))
+            
+            # create an output node
+            temp_output_node = minidom.Element("output")
+            temp_output_node.setAttribute("name", child.getAttribute("name"))
+            temp_output_node.setAttribute("path",
+                                          child.getAttribute("output_path"))
+            
+            outputFoldersNode.appendChild(temp_output_node)
+            
+            # now remove the output_path attribute from the type node
+            child.removeAttribute("output_path")
+        
+        structureNode = settings.getElementsByTagName("structure")[0]
+        
+        assert(isinstance(structureNode, minidom.Element))
+        structureNode.appendChild(outputFoldersNode)
+        
+        # now save the settings file
+        settings.writexml(
+            open(settingsFileFullPath,"w"),
+            "\t", "\t", "\n"
+        )
+        
+        # now create another sequence object showing the same sequence before
+        # and check if it is going to be able to read the file
+        new_seq = test_proj.sequences()[0]
+        
+        assert(isinstance(new_seq, project.Sequence))
+        
+        # check if for every assetType defined there is an ouput_path 
+        for asset_type in new_seq.getAssetTypes():
+            assert(isinstance(asset_type, asset.AssetType))
+            self.assertNotEqual(asset_type.output_path, "")
+        
+        # by using the dom check if the settings is converted to the new format
+        settings = minidom.parse(settingsFileFullPath)
+        
+        self.assertEquals(settings.getElementsByTagName("outputFolders"), [])
+
+
+
