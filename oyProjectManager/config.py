1405384762d633bed34f766da1f223230e9806f8
Erkan Ozgur Yilmaz <eoyilmaz@gmail.com>
2012-06-12 11:21:48 +0300

    updated config.py by renaming "Scene Assembly" to 'Layout'

--- oyProjectManager/config.py
+++ oyProjectManager/config.py
@@ -285,8 +285,8 @@ class Config(object):
                 "type_for": "Shot"
             },
             {
-                "name": "Scene Assembly",
-                "code": "SceneAss",
+                "name": "Layout",
+                "code": "Layout",
                 "path":"{{project.code}}/Sequences/{{sequence.code}}/Shots/{{version.base_name}}/{{type.code}}",
                 "filename": "{{version.base_name}}_{{version.take_name}}_{{type.code}}_v{{'%03d'|format(version.version_number)}}_{{version.created_by.initials}}{{version.extension}}",
                 "output_path": "{{version._path}}/Output/{{version.take_name}}",
