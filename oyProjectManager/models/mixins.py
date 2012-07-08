e29e88e211322885a85fe2daf21f5be2dc0934f0
Erkan Ozgur Yilmaz <eoyilmaz@gmail.com>
2012-07-08 15:31:04 +0300

    * Added the FileLink class to hold links to files in the file system. The   Version class will use this class in later versions. But for now it is   used in IOMixin.
    * Added the first ever mixin called IOMixin. It adds two new attributes to
      the  mixed in class called ``inputs`` and ``outputs``. The inputs and
      outputs attributes are holding list of FileLink instances.

--- oyProjectManager/models/mixins.py
+++ oyProjectManager/models/mixins.py
@@ -4,16 +4,81 @@
 # This module is part of oyProjectManager and is released under the BSD 2
 # License: http://www.opensource.org/licenses/BSD-2-Clause
 
+from sqlalchemy import Column, Integer, ForeignKey, Table
+from sqlalchemy.ext.declarative import declared_attr
+from sqlalchemy.orm import relationship
+from sqlalchemy.orm.mapper import validates
+from oyProjectManager.db.declarative import Base
+
+def make_plural(name):
+    """Returns the plural version of the given name argument.
+    
+    Originally developed for Stalker
+    """
+    plural_name = name + "s"
+    
+    if name[-1] == "y":
+        plural_name = name[:-1] + "ies"
+    elif name[-2:] == "ch":
+        plural_name = name + "es"
+    elif name[-1] == "f":
+        plural_name = name[:-1] + "ves"
+    elif name[-1] == "s":
+        plural_name = name + "es"
+
+    return plural_name
+
+def create_secondary_table(
+        primary_cls_name,
+        secondary_cls_name,
+        primary_cls_table_name,
+        secondary_cls_table_name,
+        secondary_table_name=None
+    ):
+    """creates any secondary table
+    """
+    
+    plural_secondary_cls_name = make_plural(secondary_cls_name)
+    
+    # use the given class_name and the class_table
+    if not secondary_table_name:
+        secondary_table_name = \
+            primary_cls_name + "_" + plural_secondary_cls_name
+    
+    # check if the table is already defined
+    if secondary_table_name not in Base.metadata:
+        secondary_table = Table(
+            secondary_table_name, Base.metadata,
+            Column(
+                primary_cls_name.lower() + "_id",
+                Integer,
+                ForeignKey(primary_cls_table_name + ".id"),
+                primary_key=True,
+            ),
+            
+            Column(
+                secondary_cls_name.lower() + "_id",
+                Integer,
+                ForeignKey(secondary_cls_table_name + ".id"),
+                primary_key=True,
+            )
+        )
+    else:
+        secondary_table = Base.metadata.tables[secondary_table_name]
+    
+    return secondary_table
+
+
 class IOMixin(object):
     """Adds the ability to be able to hold relations to :class:`~oyProjectManager.models.link.FileLink` instances.
     
-    IOMixin adds the ability to be able to be related to
+    IOMixin adds the ability of being related to
     :class:`~oyProjectManager.models.link.FileLink` instances as ``inputs`` and
     ``outputs``. It is different than a
     :class:`~oyProjectManager.models.version.Version` holding a
     :class:`~oyProjectManager.models.link.FileLink` for the source file link
     and different again from a Version holding a reference to another Version
-    instance.
+    instances.
     
     Inputs are external files used to create the outputs of the mixed in class.
     It can be and EDL file for a Project or Sequence or can be a texture file
@@ -30,6 +95,52 @@ class IOMixin(object):
     :param outputs: A list of :class:`~oyProjectManager.models.link.FileLink`
       instances holding the outputs of this mixed in class. Outputs can be a
       sequence of images lets say for a Shot Version.
+    
+    :type inputs: a list of :class:`~oyProjectManager.models.link.FileLink`
+      instances.
     """
     
+    @declared_attr
+    def inputs(cls):
+        secondary_table = create_secondary_table(
+            cls.__name__, 'FileLink', cls.__tablename__, 'FileLinks',
+            cls.__name__ + '_Inputs'
+        )
+        # return the relationship
+        return relationship('FileLink', secondary=secondary_table)
     
+    @validates('inputs')
+    def _validate_inputs(self, key, input_):
+        """validates the given input value
+        """
+        from oyProjectManager.models.link import FileLink
+        if not isinstance(input_, FileLink):
+            raise TypeError('%s.inputs should be all FileLink instances not, '
+                            '%s' % (self.__class__.__name__,
+                                    input_.__class__.__name__ ))
+        
+        return input_
+    
+    @declared_attr
+    def outputs(cls):
+        secondary_table = create_secondary_table(
+            cls.__name__, 'FileLink', cls.__tablename__, 'FileLinks',
+            cls.__name__ + '_Outputs'
+        )
+        # return the relationship
+        return relationship('FileLink', secondary=secondary_table)
+    
+    @validates('outputs')
+    def _validate_outputs(self, key, output):
+        """validates the given output value
+        """
+        from oyProjectManager.models.link import FileLink
+        if not isinstance(output, FileLink):
+            raise TypeError('%s.outputs should be all FileLink instances not, '
+                            '%s' % (self.__class__.__name__,
+                                    output.__class__.__name__ ))
+        return output
+    
+    def __init__(self, inputs=[], outputs=[]):
+        self.inputs = inputs
+        self.outputs = outputs
--- tests/mixins/test_iomixin.py
+++ tests/mixins/test_iomixin.py
@@ -14,14 +14,25 @@ from oyProjectManager.db.declarative import Base
 from oyProjectManager.models.link import FileLink
 from oyProjectManager.models.mixins import IOMixin
 
-class IOMixClass(Base, IOMixin):
+class IOMixedInClass(Base, IOMixin):
     """A class which is mixed with IOMixin for testing purposes
     """
     __tablename__ = "IOMixClasses"
     IOMixClass_id = Column("id", Integer, primary_key=True)
 
     def __init__(self, **kwargs):
-        super(IOMixClass, self).__init__(**kwargs)
+        super(IOMixedInClass, self).__init__(**kwargs)
+        IOMixin.__init__(self, **kwargs)
+
+
+class IOMixedInClass2(Base, IOMixin):
+    """A class which is mixed with IOMixin for testing purposes
+    """
+    __tablename__ = "IOMixClasses2"
+    IOMixClass_id = Column("id", Integer, primary_key=True)
+
+    def __init__(self, **kwargs):
+        super(IOMixedInClass2, self).__init__(**kwargs)
         IOMixin.__init__(self, **kwargs)
 
 class IOMixinTester(unittest.TestCase):
@@ -41,14 +52,33 @@ class IOMixinTester(unittest.TestCase):
         os.environ[conf.repository_env_key] = self.temp_projects_folder
         
         self.inputs = [
-            FileLink(filename="a.%03d.tga 5-100 13")
+            FileLink(
+                filename="a.%03d.tga 5-14 100",
+                path="/tmp"
+            ),
+            FileLink(
+                filename='b.%03d.tga 1-100',
+                path="/tmp"
+            )
         ]
         
+        self.outputs = [
+            FileLink(
+                filename='Test_Proj_Test_Seq_SH001_MAIN_Lighting_beauty_MasterBeauty.%03d.exr 1-100',
+                path='/tmp'
+            ),
+             FileLink(
+                filename='Test_Proj_Test_Seq_SH001_MAIN_Lighting_shadow_MasterBeauty.%03d.exr 1-100',
+                path='/tmp'
+            ),
+        ]
         
         self.kwargs = {
-            "inputs": "test_file_name.txt",
-            "outputs": "/some/path/to/an/unknown/place",
+            "inputs": self.inputs,
+            "outputs": self.outputs,
         }
+        
+        self.test_io_mixed_in_obj = IOMixedInClass(**self.kwargs)
     
     def tearDown(self):
         """clean up the test
@@ -61,4 +91,261 @@ class IOMixinTester(unittest.TestCase):
         shutil.rmtree(self.temp_config_folder)
         shutil.rmtree(self.temp_projects_folder)
     
+    def test_inputs_argument_is_skipped(self):
+        """testing if skipping the inputs argument will set the inputs to an
+        empty list
+        """
+        self.kwargs.pop('inputs')
+        new_obj = IOMixedInClass(**self.kwargs)
+        self.assertEqual(new_obj.inputs, [])
     
+    def test_inputs_argument_is_None(self):
+        """testing if a TypeError will be raised when the inputs argument is
+        set to None
+        """
+        self.kwargs['inputs'] = None
+        self.assertRaises(TypeError, IOMixedInClass, **self.kwargs)
+    
+    def test_inputs_attribute_is_None(self):
+        """testing if a TypeError will be raised when the inputs attribute is
+        set to None
+        """
+        self.assertRaises(
+            TypeError,
+            setattr,
+            self.test_io_mixed_in_obj.inputs,
+            None
+        )
+    
+    def test_inputs_argument_is_not_a_list_instance(self):
+        """testing if a TypeError will be raised when the inputs argument is
+        not a list
+        """
+        self.kwargs['inputs'] = 'not a list instance'
+        self.assertRaises(TypeError, IOMixedInClass, **self.kwargs)
+    
+    def test_inputs_attribute_is_not_a_list_instance(self):
+        """testing if a TypeError will be raised when the inputs attribute is
+        set to something other than a list
+        """
+        self.assertRaises(TypeError, setattr, self.test_io_mixed_in_obj,
+            'inputs', 'not a list instance')
+    
+    def test_inputs_argument_is_not_a_list_of_FileLink_instances(self):
+        """testing if a TypeError will be raised when the inputs argument is
+        not a list of all FileLink instances
+        """
+        self.kwargs['inputs'] = ['these', 'are', 'not', 'a', 'FileLink',
+                                 'instances']
+        self.assertRaises(TypeError, IOMixedInClass, **self.kwargs)
+    
+    def test_inputs_attribute_is_not_a_list_of_FileLink_instances(self):
+        """testing if a TypeError will be raised when the inputs attribute is
+        not set to a list of all FileLink instances
+        """
+        self.assertRaises(
+            TypeError,
+            setattr,
+            self.test_io_mixed_in_obj, 'inputs',
+            ['these', 'are', 'not', 'a', 'FileLink', 'instances']
+        )
+        
+    def test_inputs_argument_is_working_properly(self):
+        """testing if the inputs argument value is passed to inputs attribute
+        correctly
+        """
+        self.assertEqual(
+            self.test_io_mixed_in_obj.inputs,
+            self.kwargs['inputs']
+        )
+    
+    def test_inputs_attribute_is_working_properly(self):
+        """testing if the inputs attribute is working properly
+        """
+        new_FileLinks = [
+            FileLink('test.tga', '/tmp')
+        ]
+        self.test_io_mixed_in_obj.inputs = new_FileLinks
+        self.assertEqual(
+            self.test_io_mixed_in_obj.inputs,
+            new_FileLinks
+        )
+    
+    def test_outputs_argument_is_skipped(self):
+        """testing if skipping the outputs argument will set the outputs to an
+        empty list
+        """
+        self.kwargs.pop('outputs')
+        new_obj = IOMixedInClass(**self.kwargs)
+        self.assertEqual(new_obj.outputs, [])
+    
+    def test_outputs_argument_is_None(self):
+        """testing if a TypeError will be raised when the outputs argument is
+        set to None
+        """
+        self.kwargs['outputs'] = None
+        self.assertRaises(TypeError, IOMixedInClass, **self.kwargs)
+    
+    def test_outputs_attribute_is_None(self):
+        """testing if a TypeError will be raised when the outputs attribute is
+        set to None
+        """
+        self.assertRaises(
+            TypeError,
+            setattr,
+            self.test_io_mixed_in_obj.outputs,
+            None
+        )
+    
+    def test_outputs_argument_is_not_a_list_instance(self):
+        """testing if a TypeError will be raised when the outputs argument is
+        not a list
+        """
+        self.kwargs['outputs'] = 'not a list instance'
+        self.assertRaises(TypeError, IOMixedInClass, **self.kwargs)
+    
+    def test_outputs_attribute_is_not_a_list_instance(self):
+        """testing if a TypeError will be raised when the outputs attribute is
+        set to something other than a list
+        """
+        self.assertRaises(TypeError, setattr, self.test_io_mixed_in_obj,
+            'outputs', 'not a list instance')
+    
+    def test_outputs_argument_is_not_a_list_of_FileLink_instances(self):
+        """testing if a TypeError will be raised when the outputs argument is
+        not a list of all FileLink instances
+        """
+        self.kwargs['outputs'] = ['these', 'are', 'not', 'a', 'FileLink',
+                                 'instances']
+        self.assertRaises(TypeError, IOMixedInClass, **self.kwargs)
+    
+    def test_outputs_attribute_is_not_a_list_of_FileLink_instances(self):
+        """testing if a TypeError will be raised when the outputs attribute is
+        not set to a list of all FileLink instances
+        """
+        self.assertRaises(
+            TypeError,
+            setattr,
+            self.test_io_mixed_in_obj, 'outputs',
+            ['these', 'are', 'not', 'a', 'FileLink', 'instances']
+        )
+        
+    def test_outputs_argument_is_working_properly(self):
+        """testing if the outputs argument value is passed to outputs attribute
+        correctly
+        """
+        self.assertEqual(
+            self.test_io_mixed_in_obj.outputs,
+            self.kwargs['outputs']
+        )
+    
+    def test_outputs_attribute_is_working_properly(self):
+        """testing if the outputs attribute is working properly
+        """
+        new_FileLinks = [
+            FileLink('test.tga', '/tmp')
+        ]
+        self.test_io_mixed_in_obj.outputs = new_FileLinks
+        self.assertEqual(
+            self.test_io_mixed_in_obj.outputs,
+            new_FileLinks
+        )
+
+class IOMixin_DB_Tester(unittest.TestCase):
+    """tests IOMixin in a persistent environment
+    """
+    
+    def setUp(self):
+        """set up the test
+        """
+        conf.database_url = "sqlite://"
+        
+        # create the environment variable and point it to a temp directory
+        self.temp_config_folder = tempfile.mkdtemp()
+        self.temp_projects_folder = tempfile.mkdtemp()
+        
+        os.environ["OYPROJECTMANAGER_PATH"] = self.temp_config_folder
+        os.environ[conf.repository_env_key] = self.temp_projects_folder
+        
+        self.inputs = [
+            FileLink(
+                filename="a.%03d.tga 5-14 100",
+                path="/tmp"
+            ),
+            FileLink(
+                filename='b.%03d.tga 1-100',
+                path="/tmp"
+            )
+        ]
+        
+        self.outputs = [
+            FileLink(
+                filename='Test_Proj_Test_Seq_SH001_MAIN_Lighting_beauty_MasterBeauty.%03d.exr 1-100',
+                path='/tmp'
+            ),
+             FileLink(
+                filename='Test_Proj_Test_Seq_SH001_MAIN_Lighting_shadow_MasterBeauty.%03d.exr 1-100',
+                path='/tmp'
+            ),
+        ]
+        
+        self.kwargs = {
+            "inputs": self.inputs,
+            "outputs": self.outputs,
+        }
+        
+        self.test_io_mixed_in_obj = IOMixedInClass(**self.kwargs)
+    
+    def tearDown(self):
+        """clean up the test
+        """
+        
+        # set the db.session to None
+        db.session = None
+        
+        # delete the temp folders
+        shutil.rmtree(self.temp_config_folder)
+        shutil.rmtree(self.temp_projects_folder)
+ 
+    def test_persistence_of_IOMixin(self):
+        """testing the persistence of IOMixedInClass
+        """
+        db.setup()
+        db.session.add(self.test_io_mixed_in_obj)
+        db.session.commit()
+        
+        # now delete the object and try to retrieve it back
+        del self.test_io_mixed_in_obj
+        
+        io_mixed_in_obj_DB = db.session.query(IOMixedInClass).first()
+        
+        # check the attributes
+        self.assertEqual(
+            io_mixed_in_obj_DB.inputs, self.kwargs['inputs']
+        )
+        self.assertEqual(
+            io_mixed_in_obj_DB.outputs, self.kwargs['outputs']
+        )
+    
+    def test_another_class_mixed_in_with_IOMixin(self):
+        """testing if everything works properly if more than one class is mixed
+        in with the IOMixin
+        """
+        db.setup()
+        new_io_mixed_in_obj2 = IOMixedInClass2(**self.kwargs)
+        db.session.add(self.test_io_mixed_in_obj)
+        db.session.add(new_io_mixed_in_obj2)
+        db.session.commit()
+        
+        # delete them and retrieve back from DB
+        del new_io_mixed_in_obj2
+        del self.test_io_mixed_in_obj
+        
+        a = db.query(IOMixedInClass).first()
+        b = db.query(IOMixedInClass2).first()
+        
+        self.assertEqual(a.inputs, self.kwargs['inputs'])
+        self.assertEqual(a.outputs, self.kwargs['outputs'])
+        
+        self.assertEqual(b.inputs, self.kwargs['inputs'])
+        self.assertEqual(b.outputs, self.kwargs['outputs'])
